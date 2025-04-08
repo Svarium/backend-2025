05fe28f613a6db53d785e99bae916367b7c6a7f0
Svarium <you@example.com>
2025-04-07 21:13:07 -0300

    refactor app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+import express, { urlencoded } from "express"; //requiero express
+import dotenv from "dotenv"; //requiero dotenv
+import cors from "cors"; //requiero cors
+import fs from "node:fs"; //requiero fs 
+import morgan from "morgan";
+
+dotenv.config(); //cargo las variables de entorno
+
+const app = express(); //creo la app
+
+app.use(morgan("dev")); //uso morgan para el log de las peticiones
+
+//Middlewares
+app.use(cors({
+    origin: "*", //permito el acceso a cualquier origen
+    credentials: true, //permito el acceso a las credenciales
+}))
+app.use(express.json()); //parseo el json
+app.use(urlencoded({ //parseo el urlencoded
+    extended: true, //permito el uso de objetos y arrays
+}))
+
+
+//RUTAS
+const routeFiles = fs.readdirSync("./src/routes"); //leo la carpeta de rutas
+routeFiles.forEach((file) => {
+    //uso importaciones dinámicas
+    import(`./src/routes/${file}`).then((route) => {
+        app.use("/api/v1", route.default); //uso la ruta
+    }).catch((err) => {
+        console.error(`Error loading route ${file}:`, err); //si hay un error lo muestro por consola
+    })
+
+})
+
+
+export default app; //exporto la app
--- index.js
+++ index.js
@@ -1,37 +1,7 @@
-import express, { urlencoded } from "express"; //requiero express
-import dotenv from "dotenv"; //requiero dotenv
-import cors from "cors"; //requiero cors
-import fs from "node:fs"; //requiero fs 
+import app from "./app.js";
 
-dotenv.config(); //cargo las variables de entorno
-
-const port = process.env.PORT || 3000; //defino el puerto
-
-const app = express(); //creo la app
-
-//Middlewares
-app.use(cors({
-    origin: "*", //permito el acceso a cualquier origen
-    credentials: true, //permito el acceso a las credenciales
-}))
-app.use(express.json()); //parseo el json
-app.use(urlencoded({ //parseo el urlencoded
-    extended: true, //permito el uso de objetos y arrays
-}))
-
-
-//RUTAS
-const routeFiles = fs.readdirSync("./src/routes"); //leo la carpeta de rutas
-routeFiles.forEach((file) => {
-    //uso importaciones dinámicas
-    import(`./src/routes/${file}`).then((route) => {
-        app.use("/api/v1", route.default); //uso la ruta
-    }).catch((err) => {
-        console.error(`Error loading route ${file}:`, err); //si hay un error lo muestro por consola
-    })
-
-})
-
+// Define the port to listen on
+const port = process.env.PORT || 3005;
 
 //Inicializo Servidor
 const server = async () => {
@@ -47,4 +17,4 @@ const server = async () => {
 
 server(); //llamo a la funcion server
 
-
+console.log('Server on port: ' + port);
